d7efee461cff2411dae8dba674fa706498d3b9d2
odscv <odscv.agdi@gmail.com>
2022-08-08 01:19:27 -0500

    Login

--- app_perfiles/odsperfiles/doctype/datos_generales/datos_generales.py
+++ app_perfiles/odsperfiles/doctype/datos_generales/datos_generales.py
@@ -9,3 +9,9 @@ class DatosGenerales(Document):
 	def before_insert(self):
 		self.nombrecompleto  = self.nombres + " " + self.apellidos
 	
+def setdatosg():
+	if frappe.db.exists("User Permission", frappe.session.user):  	 
+		ent=frappe.get_doc("User Permission",frappe.session.user)
+		frappe.defaults.set_user_default("useruser", ent.user)
+		#if ent.club:
+		#	frappe.defaults.set_user_default("clubuser", ent.club)
--- app_perfiles/odsperfiles/doctype/experiencia_laboral/experiencia_laboral.js
+++ app_perfiles/odsperfiles/doctype/experiencia_laboral/experiencia_laboral.js
@@ -2,7 +2,18 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Experiencia Laboral', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.set_value('meses', monthDiff(frm.doc.fecha_ingreso,frm.doc.fecha_salida));
+		cur_frm.refresh_field(frm.doc.meses)
+	}
 });
+
+function monthDiff(d1, d2) {
+	d1 = new Date(d1);
+	d2 = new Date(d2);
+    var months;
+    months = (d2.getFullYear() - d1.getFullYear()) * 12;
+    months -= d1.getMonth();
+    months += d2.getMonth();
+    return months <= 0 ? 0 : months;
+}
